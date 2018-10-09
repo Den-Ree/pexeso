8054cdbf5de0b72b3f170a134760f0e7a2600dd3
Den Ree <denis.rusakovich@blanco.services>
2018-10-09 14:35:01 +0200

    Setup the project

---
+++ Pexeso/Pexeso/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Pexeso
+//
+//  Created by Den Ree on 09/10/2018.
+//  Copyright © 2018 Den Ree. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
---
+++ Pexeso/Pexeso/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Pexeso
+//
+//  Created by Den Ree on 09/10/2018.
+//  Copyright © 2018 Den Ree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
